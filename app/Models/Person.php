39f55ab60dc174213890e8efec41450e7b14316b
AbdulBasitx19 <abdulbasitchaudhry106@gmail.com>
2026-09-09 20:01:27 +0500

    feat: defin HasMany and BelongsTo relationships in models

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Person extends Model
 {
     //
+    protected $fillable = [
+        'name' ,
+        'age',
+    ];
+
+    public function conatcts(): HasMany
+    {
+        return $this->hasMany(Contact::class);
+    }
 }
